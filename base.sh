a817f7dcb801a30d9ee57f19236dcbf6bf6364eb
felixdumaslavoie <felixdumaslavoie@gmail.com>
2024-10-03 20:52:49 -0400

    Ca avance...

--- base.sh
+++ base.sh
@@ -123,8 +123,42 @@ adduser flx
 
 passwd flx
 
-### NEOVIM Instalation section ###
+## Making sure git is there...
+dnf -y install git
+
+## NEOVIM Instalation section ###
+# Sources:
+# https://fedoramagazine.org/configuring-neovim-on-fedora-as-an-ide-and-using-lazyvim/
+# https://www.lazyvim.org/installation
+# required
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+#
+# Main installation
 dnf -y install neovim
-dnf
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+## Installing kitty (my fav' terminal) <^-^>
+# Source: https://sw.kovidgoyal.net/kitty/binary/
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+## desktop integration on Linux part...
+# Create symbolic links to add kitty and kitten to PATH (assuming ~/.local/bin is in
+# your system-wide PATH)
+ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+# Place the kitty.desktop file somewhere it can be found by the OS
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
+cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+# Update the paths to the kitty and its icon in the kitty desktop file(s)
+sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+# Make xdg-terminal-exec (and hence desktop environments that support it use kitty)
+echo 'kitty.desktop' >~/.config/xdg-terminals.list
 
 #disable virtuals desktops option called "overview" in KDE desktop effects settings page. Just turn it off...
