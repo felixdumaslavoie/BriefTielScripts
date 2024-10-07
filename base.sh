555ae877b9a50d1ef808c491b21e87a9600ad316
felixdumaslavoie <felixdumaslavoie@gmail.com>
2024-10-07 15:57:12 -0400

    un autre :p

--- base.sh
+++ base.sh
@@ -124,7 +124,7 @@ dnf -y —refresh upgrade
 
 ### Shell parts 01010101 XO
 ## Making sure everything is there...
-dnf -y install adduser git wget ranger eza zsh htop btop ripgrep
+dnf -y install adduser git wget ranger eza zsh htop btop ripgrep vifm
 
 adduser flx
 # https://stackoverflow.com/questions/714915/using-the-passwd-command-from-within-a-shell-script
